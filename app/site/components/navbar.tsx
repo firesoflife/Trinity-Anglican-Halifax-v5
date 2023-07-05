60a3a19a3251f2bfaf73d87c22fc19759301836f
firesoflife <bryangoertz@gmail.com>
2023-07-05 11:02:58 -0400

    fixed type for some JS variables1

--- app/site/components/navbar.tsx
+++ app/site/components/navbar.tsx
@@ -4,16 +4,18 @@ import Link from 'next/link'
 import Image from 'next/image'
 import Church_Crest from '../../../public/Church_Crest.png'
 import { useState, useEffect, useRef } from 'react'
+import { FaChevronDown } from 'react-icons/fa';
+
 
 const Navbar = () => {
   const logo = Church_Crest;
-  const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
+  const [openDropdown, setOpenDropdown] = useState<string | null>(null);
   const submenuRef = useRef(null);
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (submenuRef.current && !(submenuRef.current as Element).contains(event.target as Element)) {
-        setIsSubMenuOpen(false);
+        setOpenDropdown(null);
       }
     };
   
@@ -23,12 +25,12 @@ const Navbar = () => {
     };
   }, [submenuRef]);
 
-  const handleSubMenuClick = () => {
-    setIsSubMenuOpen(!isSubMenuOpen);
+  const handleSubMenuClick = (dropdownName: string) => {
+    setOpenDropdown(openDropdown === dropdownName ? null : dropdownName);
   }
 
   const handleLinkClick = () => {
-    setIsSubMenuOpen(false);
+    setOpenDropdown(null);
   }
 
   return (
@@ -44,22 +46,72 @@ const Navbar = () => {
         </div>
       </div>
       <div className="navbar-end">
-        <ul className="menu menu-horizontal space-x-3 text-secondary font-mainContent flex items-center">
-          <li className="hover:text-secondaryHover cursor-pointer">About</li>
+        <ul className="space-x-3 text-secondary font-mainContent flex items-center">
           <li className="relative" ref={submenuRef}>
-            <a className="hover:text-secondaryHover cursor-pointer" onClick={handleSubMenuClick}>Parent</a>
-            <ul className={`absolute top-full left-0 bg-cyan-50 p-2 ${isSubMenuOpen ? 'block' : 'hidden'}`}>
-              <li><a onClick={handleLinkClick}>Submenu 1</a></li>
-              <li><a onClick={handleLinkClick}>Submenu 2</a></li>
+            <div className="flex items-center space-x-1 cursor-pointer hover:text-secondaryHover" onClick={() => handleSubMenuClick('about')}>
+              <p>About</p>
+              <FaChevronDown />
+            </div>
+            <ul className={`absolute top-full left-0 bg-cyan-50 ${openDropdown === 'about' ? 'block' : 'hidden'} whitespace-nowrap`}>
+              <li className="my-2 hover:bg-secondary hover:text-primary cursor-pointer">
+                <Link href="#">
+                  <div className="block px-4" onClick={handleLinkClick}>
+                    Our History
+                  </div>
+                </Link>
+              </li>
+              <li className="my-2 hover:bg-secondary hover:text-primary cursor-pointer">
+                <Link href="#">
+                  <div className="block px-4" onClick={handleLinkClick}>
+                    Clergy & People
+                  </div>
+                </Link>
+              </li>
+              <li className="my-2 hover:bg-secondary hover:text-primary cursor-pointer">
+                <Link href="#">
+                  <div className="block px-4" onClick={handleLinkClick}>
+                    Location
+                  </div>
+                </Link>
+              </li>
             </ul>
           </li>
-          <li className="hover:text-secondaryHover cursor-pointer">Worship</li>
+          <li className="relative" ref={submenuRef}>
+            <div className="flex items-center space-x-1 cursor-pointer hover:text-secondaryHover" onClick={() => handleSubMenuClick('worship')}>
+              <p>Worship</p>
+              <FaChevronDown />
+            </div>
+            <ul className={`absolute top-full left-0 bg-cyan-50 ${openDropdown === 'worship' ? 'block' : 'hidden'} whitespace-nowrap`}>
+              <li className="my-2 hover:bg-secondary hover:text-primary cursor-pointer">
+                <Link href="#">
+                  <div className="block px-4" onClick={handleLinkClick}>
+                    What to Expect
+                  </div>
+                </Link>
+              </li>
+              <li className="my-2 hover:bg-secondary hover:text-primary cursor-pointer">
+                <Link href="#">
+                  <div className="block px-4" onClick={handleLinkClick}>
+                    Worship Schedule
+                  </div>
+                </Link>
+              </li>
+              <li className="my-2 hover:bg-secondary hover:text-primary cursor-pointer">
+                <Link href="#">
+                  <div className="block px-4" onClick={handleLinkClick}>
+                    Sermons & Teaching
+                  </div>
+                </Link>
+              </li>
+            </ul>
+          </li>
           <li className="hover:text-secondaryHover cursor-pointer">Parish Life</li>
           <li className="hover:text-secondaryHover cursor-pointer">Facility Rental</li>
           <li className="hover:text-secondaryHover cursor-pointer">Contact</li>
         </ul>
       </div>
     </nav>
+   
   )
 }
 
