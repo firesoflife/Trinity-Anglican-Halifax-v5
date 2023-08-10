496c51cf93c75382fdbad619f79322fce88e9237
firesoflife <bryangoertz@gmail.com>
2023-08-10 08:45:26 -0400

    added welcome section to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import ImageUrlBuilder from '@sanity/image-url';
 import 'app/globals.css';
 import { fallbackImages } from './site/utilities/fallbackAssets';
 import { getHome } from './lib/api/getHome';
+import QuickLinksLayout from './site/components/QuickLinksLayout';
 
 const builder = ImageUrlBuilder(client);
 
@@ -11,24 +12,42 @@ function urlFor(source: SanityImageSource) {
 	return builder.image(source);
 }
 
-// Hero Image
-const welcomeImageUrl =
-	// home.welcomeImage ? urlFor(home.welcomeImage).url() :
-	fallbackImages.heroImageFallback;
-
 const HomePage = async () => {
 	const home = await getHome();
 
-	console.log(home.pageTitle);
+	// Hero Image
+	const welcomeImageUrl = home.welcomeImage
+		? urlFor(home.welcomeImage).url()
+		: fallbackImages.heroImageFallback;
 
 	return (
 		<div className='font-sans antialiased bg-primary text-gray-900'>
 			<div className='bg-primary'>
 				<section
-					className='hero h-[75vh]'
-					style={{ backgroundImage: `url(${welcomeImageUrl})` }}
-				/>
-				<h1>Content lives here: {home.pageTitle} </h1>
+					className='hero h-[75vh] '
+					style={{ backgroundImage: `url(${welcomeImageUrl})` }}>
+					<div className='hero-overlay bg-opacity-60 border-b-[1px] border-primary'></div>
+					<div className='hero-content text-center text-neutral-content'>
+						<div className='max-w-md'>
+							<div className='mb-96 text-5xl font-bold font-heading leading-snug'>
+								{home.pageTitle}
+							</div>
+						</div>
+					</div>
+				</section>
+				<section className='px-8 py-16 bg-secondary'>
+					<h2 className='text-primary text-center text-4xl pb-4 font-subheading'>
+						{home.welcomeHeading}
+					</h2>
+					<div className='max-w-4xl m-auto'>
+						<p className='leading-8 text-xl text-primary text-center font-subContent'>
+							{home.welcome}
+						</p>
+					</div>
+				</section>
+				<section>
+					<QuickLinksLayout />
+				</section>
 			</div>
 		</div>
 	);
---
+++ app/site/components/QuickLinksLayout.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function QuickLinksLayout() {
+	return <div>QuickLinksLayout</div>;
+}
+
+export default QuickLinksLayout;
