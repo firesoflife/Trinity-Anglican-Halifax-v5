824efc79a69ca551ea83c6257a59ec36d3ed901c
firesoflife <bryangoertz@gmail.com>
2024-05-11 21:59:04 -0400

    change sermon groq to order by desc

--- app/lib/api/getSermons.ts
+++ app/lib/api/getSermons.ts
@@ -1,10 +1,11 @@
 import { groq } from 'next-sanity';
 import { client } from '@/sanity/lib/client';
+import { Sermon } from '@/typings';
 
 export async function getSermons(): Promise<Sermon[]> {
 	return client.fetch(
 		groq`
-        *[_type == "sermon"]{
+        *[_type == "sermon"] | order(date desc){
             ...,
             title,
             slug,
